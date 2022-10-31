2d4eda93eea9c9d9035f0d7059e89ef0115f3d29
Senija <skaleta1@etf.unsa.ba>
2022-10-31 23:06:17 +0100

    add footer

--- auction-frontend/src/App.js
+++ auction-frontend/src/App.js
@@ -1,12 +1,14 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       
       <Header/>
+      <Footer/>
     </div>
   );
 }
--- auction-frontend/src/components/Header.js
+++ auction-frontend/src/components/Header.js
@@ -16,15 +16,31 @@ import { purple } from "@mui/material/colors";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
+import InputBase from "@mui/material/InputBase";
+import Paper from "@mui/material/Paper";
 
 const Header = () => {
   return (
     <>
       <Navbar bg="dark" variant="light">
         <Container>
-          <FacebookIcon fontSize="medium" sx={{ color: grey[50] }} />
-          <InstagramIcon fontSize="medium" sx={{ color: grey[50] }} />
-          <TwitterIcon fontSize="medium" sx={{ color: grey[50] }} />
+          <a href="https://www.facebook.com/">
+            <FacebookIcon fontSize="medium" sx={{ color: grey[50] }} />
+          </a>
+          <a href="https://www.instagram.com/">
+            <InstagramIcon fontSize="medium" sx={{ color: grey[50] }} />
+          </a>
+          <a href="https://twitter.com/">
+            <TwitterIcon fontSize="medium" sx={{ color: grey[50] }} />
+          </a>
+          {/* <Link
+            href="http://www.google.com"
+            className={classes.link}
+            target="_blank"
+          >
+            <SearchIcon />
+          </Link> */}
+
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text style={{ color: grey[50] }}>Hi, John Doe</Navbar.Text>
@@ -38,20 +54,44 @@ const Header = () => {
             <Navbar.Text style={{ color: purple[200] }}> AUCTION</Navbar.Text>
           </Navbar.Brand>
           {/* <form> */}
-          <TextField
-            fullWidth
-            id="fullWidth"
-            type="text"
-            label="Try enter: Shoes"
-          />
-          <IconButton type="submit">
-            <SearchIcon sx={{ color: grey[500] }} />
-          </IconButton>
+
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 1,
+            }}
+          >
+            {/* <TextField
+              fullWidth
+              id="fullWidth"
+              type="text"
+              label="Try enter: Shoes"
+              variant="standard"
+            /> */}
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Try enter: Shoes"
+              inputProps={{ "aria-label": "Try enter: Shoes" }}
+            />
+
+            <IconButton type="submit">
+              <SearchIcon sx={{ color: grey[500] }} />
+            </IconButton>
+          </Paper>
           {/* </form> */}
           <Nav className="me-auto">
-            <Nav.Link className="navLink" href="#home" >HOME</Nav.Link>
-            <Nav.Link className="navLink" href="#shop" >SHOP</Nav.Link>
-            <Nav.Link className="navLink" href="#myaccount" >MY ACCOUNT</Nav.Link>
+            <Nav.Link className="navLink" href="#home">
+              HOME
+            </Nav.Link>
+            <Nav.Link className="navLink" href="#shop">
+              SHOP
+            </Nav.Link>
+            <Nav.Link className="navLink" href="#myaccount">
+              ACCOUNT
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
