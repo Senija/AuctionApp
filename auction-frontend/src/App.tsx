b1e3f8107ddc777770132118bd416f7f293cc9b9
Senija <skaleta1@etf.unsa.ba>
2022-11-06 21:09:16 +0100

    add feature footer

--- auction-frontend/src/App.tsx
+++ auction-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Router>
         
         <Header/>
+        <Footer/>
         {/* <Routes>
           <Route path="/termsAndConditions" element={<TermsAndCOnditions />}></Route>
         </Routes>
--- auction-frontend/src/components/Footer.tsx
+++ auction-frontend/src/components/Footer.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
-import Navbar from 'react-bootstrap/Navbar';
+import Button from "@mui/material/Button";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Link } from "react-router-dom";
+import "../assets/style/Footer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="footerMain">
+        <Container>
+          <div className="container">
+            <div className="title">
+              AUCTION <br /> <br />
+              <Link className="link" to="/about">
+                {" "}
+                About us
+              </Link>{" "}
+              <br />
+              <Link className="link" to="termsandconditions">
+                {" "}
+                Terms and Conditions
+              </Link>
+              <br />
+              <Link className="link" to="privacyandpolicy">
+                {" "}
+                Privacy and Policy
+              </Link>
+              <br />
+            </div>
+            <div className="title">
+              GET IN TOUCH
+              <br /> <br />
+              <p className="link">
+                Call us at +123 797-567-2535
+                <br />
+                <a className="link" href="mailto: support@auction.com">
+                  support@auction.com
+                </a>
+              </p>
+            </div>
+            <div className="title">
+              NEWSLETTER
+              <br /> <br />
+              <p className="link">
+                Enter your email adress and get notified about <br></br> new
+                products. We hate spam!
+              </p>
+              <div className="containerInput">
+              <form>
+                <input className="inputF" type="text" placeholder='Your email adress'></input>
+                <Button variant="outlined" color="secondary" sx={{margin: 2, alignItems: 'center'}} endIcon={<ArrowForwardIosIcon />}>Go </Button>
+              </form>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
